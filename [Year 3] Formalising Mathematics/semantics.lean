bcaceba12aa165aedb3aadadb759579dc4277b36
Jiaru Li <eric.jiaru.li@gmail.com>
2026-02-03 13:23:09 +0800

    1

--- [Year 3] Formalising Mathematics/semantics.lean
+++ [Year 3] Formalising Mathematics/semantics.lean
@@ -1,54 +1,123 @@
-import formula
+import syntax
+import typing_relation
+import category_theory.closed.cartesian
+import category_theory.limits.shapes.binary_products
 
-/- The soundness and completeness theorems are always proven with respect to a 
-semantics. In this file we define the usual truth-value based reasoning 
-system, where formulas are evaluated to the truth value they denote. The 
-evaluation is based on what truth values are assigned to the variables, so a
-semantic model of propositional logic in this system is a truth assignment 
-function to the propositional variables. We also define some related notions 
-such as satisfiability.
--/
+open term type typing_relation
+open category_theory category_theory.limits
 
-variable {vars : Type}
+variables {con gnd fv : Type} [fvar fv] [const con gnd]
 
-/-- Evaluates formulas to truth values given a propositional variable 
-    truth assignment `v`. -/
-def eval (v : vars → bool) : Form vars → bool
-| ⊥       := ff
-| ⦃x⦄     := v x
-| (~ P)   := not (eval P)
-| (P ⋀ Q) := and (eval P) (eval Q)
-| (P ⋁ Q) := or (eval P) (eval Q)
+variables {𝓒 : Type} [category 𝓒] 
+          [limits.has_finite_products 𝓒] [cartesian_closed 𝓒]
 
-notation `⟦` P `⟧_` v := eval v P
-notation v ` ⊨ ` A := ⟦A⟧_v
-notation v ` ⊨ ` Γ := ∀ γ, γ ∈ Γ → ⟦γ⟧_v
+--noncomputable because it uses limits.has_limit, which uses classical.choice
+noncomputable def eval_type (G : gnd → 𝓒) : type gnd → 𝓒 :=
+begin
+  refine type.rec _ _ _ _,
+  exact G,
+  exact ⊤_𝓒,
+  { intros A1 A2 rec1 rec2,
+    exact rec1 ⨯ rec2, },
+  { intros A1 A2 rec1 rec2,
+    exact rec1 ⟹ rec2 }
+end
 
-theorem no_bot (v : vars → bool) : ¬ (↥(v ⊨ ⊥)) :=
-by simp [eval]
+notation M `⟦` A `⟧` := eval_type M A
 
-/-- A set of formulas Γ semantically entail another formula A iff 
-all the models of Γ are models of A. -/
-def entail (Γ : set (Form vars)) (A : Form vars) : Prop :=
-∀ (v : vars → bool), (v ⊨ Γ) → (v ⊨ A)
+noncomputable def eval_env (G : gnd → 𝓒) : env gnd fv → 𝓒 :=
+begin
+  refine list.rec _ _,
+  exact ⊤_𝓒,
+  { rintros ⟨x, A⟩ Γ rec,
+    exact eval_type G A ⨯ rec}
+end
 
-notation Γ ` ⊨ ` A := entail Γ A
-notation Γ ` ⊭ ` A := ¬ entail Γ A
+notation M `⟦` Γ `⟧` := eval_env M Γ
 
-/-- A set of formulas are satisfiable iff it has a model. -/
-def satisfiable (Γ : set (Form vars)) : Prop :=
-∃ (v : vars → bool), v ⊨ Γ
+structure model (gnd con 𝓒: Type) [const con gnd] 
+[category 𝓒] [limits.has_finite_products 𝓒] [cartesian_closed 𝓒] :=
+(G : gnd → 𝓒)
+(C : Π c: con, ⊤_𝓒 ⟶ G⟦const.type_of c⟧)
 
-/-- A set of formulas are satisfiable iff it has no model. -/
-def unsatisfiable (Γ : set (Form vars)) : Prop :=
-¬ satisfiable Γ
+variables {Γ : env gnd fv} {t : term gnd con fv} {A : type gnd}
 
-variable {Γ : set (Form vars)}
+set_option pp.proofs true
 
-theorem satisfiable_iff : satisfiable Γ ↔ (Γ ⊭ ⊥) :=
-by simp [satisfiable, entail, eval]
+noncomputable def eval_has_type
+(M : model gnd con 𝓒)
+: (Γ ⊩ t ∷ A) → (M.G⟦Γ⟧ ⟶ M.G⟦A⟧) :=
+begin
+  -- the recursion syntax outside tactic mode gives a non-wf recursion error,
+  -- so I use tactic mode
+  intros 𝓙,
+  induction 𝓙,
+  case has_type.Fvar : Γ x A {
+    -- M⟦Γ⟧ ⨯ M⟦A⟧ -π₂-> M⟦A⟧
+    exact limits.prod.fst
+  },
+  case has_type.Fvar' : Γ x y A A' _ _ rec rec_ret {
+    -- M⟦Γ⟧ ⨯ M⟦A'⟧ -π₁-> M⟦Γ⟧ -rec_ret → M⟦A⟧
+    exact limits.prod.snd ≫ rec_ret,
+  },
+  case has_type.Const : Γ c {
+    -- M⟦Γ⟧ -⟨⟩-> ⊤ -M.C-> M⟦con_type c⟧
+    exact terminal.from (M.G⟦Γ⟧) ≫ M.C c
+  },
+  case has_type.Unit : Γ {
+    -- M⟦Γ⟧ -⟨⟩-> ⊤
+    exact terminal.from (M.G⟦Γ⟧)
+  },
+  case has_type.Pair : Γ t t' A A' rec rec' rec_ret rec_ret' {
+    -- M⟦Γ⟧ -⟨rec_ret, rec_ret'⟩-> M⟦A ∏ A'⟧
+    exact prod.lift rec_ret rec_ret',
+  },
+  case has_type.Fst : Γ t A A' rec rec_ret {
+    -- M⟦Γ⟧ -rec_ret-> M⟦A ∏ A'⟧ -fst-> M⟦A⟧
+    exact rec_ret ≫ limits.prod.fst,
+  },
+  case has_type.Snd : Γ t A A' rec rec_ret {
+    -- M⟦Γ⟧ -rec_ret-> M⟦A ∏ A'⟧ -fst-> M⟦A'⟧
+    exact rec_ret ≫ limits.prod.snd,
+  },
+  case has_type.Abs : Γ t A A' rec rec_ret {
+    -- M⟦A⟧ ⨯ M⟦Γ⟧ -rec_ret x hfresh-> M⟦A'⟧
+    -- so M⟦Γ⟧ -curry (rec_ret x hfresh)-> (M⟦A⟧ ⟹ M⟦A'⟧)
+    set x := fvar.fresh (free_vars t ∪ (list.keys Γ).to_finset),
+    have hfresh := fvar.hfresh (free_vars t ∪ (list.keys Γ).to_finset),
+    exact cartesian_closed.curry (rec_ret x hfresh)
+  },
+  case has_type.App : Γ t t' A A' rec rec' rec_ret rec_ret' {
+    -- M⟦Γ⟧ -⟨rec_ret', rec_ret⟩-> (M⟦A⟧ ⟹ M⟦A'⟧) ⨯ M⟦A⟧ -eval-> M⟦A'⟧
+    exact prod.lift rec_ret' rec_ret ≫ (exp.ev (M.G⟦A⟧)).app (M.G⟦A'⟧)
+  }
+end
 
-theorem unsatisfiable_iff : unsatisfiable Γ ↔ (Γ ⊨ ⊥) :=
-by simp [unsatisfiable, satisfiable_iff]
+-- noncomputable def eval_has_type'
+-- (M : model gnd con 𝓒) : ∀ {Γ : env gnd fv} {t : term gnd con fv} {A : type gnd},
+-- (Γ ⊩ t ∷ A) → (M.G⟦Γ⟧ ⟶ M.G⟦A⟧)
+-- | (⟨_, A⟩ :: Γ) ⌊x⌋ _ (has_type.Fvar _) := limits.prod.fst
+-- | (⟨y, A'⟩ :: Γ) ⌊x⌋ A (has_type.Fvar' _ _ rec) :=
+-- limits.prod.snd ≫ eval_has_type' rec
+-- | Γ |c| A (has_type.Const _) := terminal.from (M.G⟦Γ⟧) ≫ M.C c
+-- | Γ ⟪⟫ unit (has_type.Unit _) := terminal.from (M.G⟦Γ⟧)
+-- | Γ ⟪t1, t2⟫ (A ∏ B) (has_type.Pair rec1 rec2) := 
+-- prod.lift (eval_has_type' rec1) (eval_has_type' rec2)
+-- | Γ (fst t) A (has_type.Fst rec) := 
+-- eval_has_type' rec ≫ by { unfold eval_type, exact limits.prod.fst}
+-- | Γ (snd t) B (has_type.Snd rec) :=
+-- eval_has_type' rec ≫ by { unfold eval_type, exact limits.prod.snd}
+-- | Γ (Λ A. t) (_ ⊃ B) (has_type.Abs rec) := 
+-- by {
+--   have hfresh := fvar.hfresh (free_vars t ∪ (list.keys Γ).to_finset),
+--   set x := fvar.fresh (free_vars t ∪ (list.keys Γ).to_finset),
+--   have rec_ret := eval_has_type' (rec x hfresh),
+--   exact cartesian_closed.curry rec_ret}
+-- | Γ (t1 ⬝ t2) A1 (@has_type.App _ _ _ _ _ _ _ _ A2 _ rec1 rec2) :=
+-- by {
+--   have rec_ret1 := eval_has_type' rec1, unfold eval_type at rec_ret1,
+--   have rec_ret2 := eval_has_type' rec2,
+--   exact prod.lift rec_ret2 rec_ret1 ≫ (exp.ev (M.G⟦A2⟧)).app (M.G⟦A1⟧)
+-- }
 
-#lint
+notation M `⟦` 𝓙 `⟧` := eval_has_type M 𝓙
